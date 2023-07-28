573ee9c824aea6b6b335942bf6b95288ee5449a8
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-07-28 12:26:56 +0300

    fix

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
-const { v4: uuidv4 } = require("crypto");
+const { randomUUID } = require("crypto");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
@@ -41,7 +41,7 @@ async function addContact(name, email, phone) {
   try {
     const data = await fs.readFile(contactsPath, "utf-8");
     const contacts = JSON.parse(data);
-    const newContact = { id: uuidv4(), name, email, phone };
+    const newContact = { id: randomUUID(), name, email, phone };
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return newContact;
